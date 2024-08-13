5a4d68332b0527821e796dca778e3c0904e4151a
Renu Yadav <renuya886@gmail.com>
2024-08-13 10:02:20 +0530

    have_changed

--- Api.js
+++ Api.js
@@ -1,8 +1,15 @@
-fetch("https://dummyjson.com/products").then((res)=>{
-    // console.log(res);    
-// console.log(res.json()) 
-res.json().then((data)=>{
-    console.log(data);
-})
+// fetch("https://dummyjson.com/products").then((res)=>{
+//     // console.log(res);    
+// // console.log(res.json()) 
+// res.json().then((data)=>{
+//     console.log(data);
+// });
    
+// });
+
+// this is without nesting
+fetch("https://dummyjson.com/products").then((res)=>{
+    return res.json()
+}).then((data)=>{
+    console.log(data);
 })
